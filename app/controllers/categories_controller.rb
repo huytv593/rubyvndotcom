5206ec505ef14c05b675e861327e872e805e0db0
huytv <huytv593@gmail.com>
2016-07-04 22:22:26 +0700

    get post by page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
-    @posts = Post.where(category_id: @category.id)
+    @posts = Post.where(category_id: @category.id).page params[:page]
   end
 end
